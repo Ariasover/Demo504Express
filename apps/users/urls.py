d5265782aa0d220661191600e983490a9f127cff
Ariasover <ariasover@gmail.com>
2021-02-02 00:57:34 -0600

    added user configuration

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,12 +4,13 @@
 from django.urls import path
 
 # Views
-from .views.users import LoginView,LogoutView,IndexView
+from .views.users import *
 
 urlpatterns = [
     path('',LoginView.as_view(),name="login"),
     path('users/logout',LogoutView.as_view(),name="logout"),
     path('users/index',IndexView.as_view(),name="index"),
+    path('users/list',UsersListView.as_view(),name="users_list"),
     # path('users/signup',UserSignupAPIView.as_view(),name="signup"),
     # path('users/verify',AccountVerificationAPIView.as_view(),name="verify"),
 ]
--- apps/users/views/users.py
+++ apps/users/views/users.py
@@ -2,11 +2,13 @@
 
 # Django 
 from django.contrib.auth import authenticate,logout,login
-from django.views.generic import View
+from django.views.generic import View,ListView,UpdateView,CreateView,DeleteView,DetailView
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.conf import settings
 from django.urls import reverse
+from django.contrib.auth.models import User
+
 
 # Model
 from apps.messagesconf.models.messagesconf import ChromeDriverVerification
@@ -20,8 +22,6 @@ from selenium import webdriver
 
 # ChromeDriver
 import chromedriver_autoinstaller
-
-
 
 
 class LoginView(View):
@@ -57,51 +57,17 @@ class IndexView(View):
 		# Comprobar speech activo.
 		
 		try:
-			print('verificar')
 			chromedriver_autoinstaller.install()
-			print('verificar2',chromedriver_autoinstaller)
-			
-				
-				# plistloc = "/Applications/Google Chrome.app/Contents/Info.plist"
-				# pl = plistlib.readPlist(plistloc)
-				# chrome_server_version = pl["CFBundleShortVersionString"]
-				# chrome_server_version = chrome_server_version[0]+chrome_server_version[1] #for example '87'	
-				# print('aqui1')			
-				# chrome_online_version = requests.get('https://chromedriver.storage.googleapis.com/LATEST_RELEASE_'+chrome_server_version) #Example 87.43.44.4
-				# print('aqui2')
-				# chrome_database_version = ChromeDriverVerification.objects.filter(version=str(chrome_server_version)).last()
-				# print('aqui3')
-				# if not str(chrome_server_version) == str(chrome_database_version):
-					
-				# 	r = requests.get('https://chromedriver.storage.googleapis.com/'+chrome_online_version.text+'/chromedriver_mac64.zip')
-				# 	print('no puedo concatener')
-
-			# ChromeDriverVerification.objects.create(version=str(chrome_server_version))
-			# z = zipfile.ZipFile(io.BytesIO(r.content))
-			# z.extractall(settings.BASE_DIR)
-
-
-			# print(str(settings.BASE_DIR)+'/chromedriver')
-			# /Users/ariasover/Documents/Proyectos/504ExpressEnv/
-			# # Verificar la version de chrome actual
-			# # chrome_path = 'C:\Program Files\Google\Chrome\Application'
-			# plistloc = "/Applications/Google Chrome.app/Contents/Info.plist"
-			# las_version_on_db = ChromeDriverVerification.objects.filter(version=str(online_version.text)).last()
-			# # if not str(las_version_on_db) == str(online_version.text):
-			# print('son diferentes')
-			# 	# if platform == "linux":
-			# 		# r÷ = requests.get('https://chromedriver.storage.googleapis.com/'+response.text+'/chromedriver_linux64.zip')
-			# 	# elif platform == "darwin":
-			# r = requests.get('https://chromedriver.storage.googleapis.com/'+'87.0.4280.88'+'/chromedriver_mac64.zip')
-			# 	# else:
-			# 		# r = requests.get('https://chromedriver.storage.googleapis.com/'+response.text+'/chromedriver_win32.zip')
-				
-			# 	# ChromeDriverVerification.objects.create(version=str(response.text))
-
-			# z = zipfile.ZipFile(io.BytesIO(r.content))
-			# z.extractall(settings.BASE_DIR)
-			
 		except Exception as e:
 			print('ERROR',e)
 		
 		return render(request, 'index.html')
+
+
+class UsersListView(ListView):
+    	
+	model = User
+	template_name = 'users-list.html'
+	queryset = User.objects.filter(is_active=True)
+	context_object_name = 'users_list'
+	paginate_by = 4
